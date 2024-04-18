8df056307ef9002a2e3bf3297052c6a21377010a
Yashas S P <yashassp2@gmail.com>
2024-04-18 20:49:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CityStateSelector from "./components/CityStateSelector";
+
+function App() {
+  return (
+    <div>
+      <CityStateSelector />
+    </div>
+  );
+}
+
+export default App;
